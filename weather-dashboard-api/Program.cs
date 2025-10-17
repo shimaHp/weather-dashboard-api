329c7353a5ddc24b7e0d7a9d00f712757793dc03
ShimaHP <ShimaHP@DESKTOP-DU8NA7B>
2025-10-17 14:12:17 +0100

    add settings to program.cs

--- weather-dashboard-api/Program.cs
+++ weather-dashboard-api/Program.cs
@@ -1,12 +1,32 @@
+using weather_dashboard_api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Memory Cache
+builder.Services.AddMemoryCache();
+
+// Add HttpClient with HttpClientFactory pattern
+builder.Services.AddHttpClient<IWeatherService, WeatherService>();
+
+// Register WeatherService
+builder.Services.AddScoped<IWeatherService, WeatherService>();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
